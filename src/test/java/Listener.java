8b823150d7b6653856c2594528a78fe01117537d
abidm-bit <abidmres@gmail.com>
2025-03-18 14:14:42 -0400

    listener class

--- src/test/java/Listener.java
+++ src/test/java/Listener.java
@@ -11,7 +11,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
-public class Listener extends Base implements ITestListener{
+public class Listener implements ITestListener{
     @Override
     public void onTestStart(ITestResult result) {
         System.out.println("Test started");
@@ -19,11 +19,11 @@ public class Listener extends Base implements ITestListener{
 
     @Override
     public void onTestFailure(ITestResult result) {
+        DateFormat dateFormat = new SimpleDateFormat("MMM/dd/yyyy/hh:mm aaa");
+        Date date = new Date();
+        String name = dateFormat.format(date);
+        File file = ((TakesScreenshot) Base.driver).getScreenshotAs(OutputType.FILE);
         try {
-            DateFormat dateFormat = new SimpleDateFormat("MMM/dd/yyyy/hh:mm aaa");
-            Date date = new Date();
-            String name = dateFormat.format(date);
-            File file = ((TakesScreenshot) Base.driver).getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(file, new File("src/test/screenshots/failedTests/" + name +".png"));
         } catch (IOException e) {
             System.out.println("Test failed");
